22acdfb54cfb440cd193c92d3229b4956871f407
JulietKocharian <julietakocharian@gmail.com>
2024-03-10 22:13:06 +0400

    phone click added

--- src/components/buttons/PhoneButton/PhoneButton.jsx
+++ src/components/buttons/PhoneButton/PhoneButton.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import styles from './phoneButton.module.css';
 
 const PhoneButton = () => {
+  const _phoneClick = () => {
+    const phoneNumber = '+37493234061';
+    window.open(`tel:${phoneNumber}`, '_self');
+  };
   return (
-    <div className={styles.phoneContainer}>
-    <button className={styles.phoneButton}>
+    <button className={styles.phoneButton} onClick={_phoneClick}>
         <img src='../assets/headers/menu/phoneIcon.svg' alt='phoneButton'/>
     </button>
-    </div>
   )
 }
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import HeaderSection from '../../view/header/headerSection';
-import FirstSection from '../../view/sections/firstSection/firstSection';
-import SecondSection from '../../view/sections/secondSection/secondSection';
 import ThirdSection from '../../view/sections/thirdSection/thirdSection';
-import FourthSection from '../../view/sections/fourthSection/fourthSection';
 import FifthSection from '../../view/sections/fifthSection/fifthSection';
+import FirstSection from '../../view/sections/firstSection/firstSection';
 import PhoneButton from '../../components/buttons/PhoneButton/PhoneButton';
+import SecondSection from '../../view/sections/secondSection/secondSection';
+import FourthSection from '../../view/sections/fourthSection/fourthSection';
 
 const Home = () => {
   return (
